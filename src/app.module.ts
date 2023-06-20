59d1aa8b4e6589f1fb31e7f697ba35caa05e7652
akshayDhingra01 <a4akshayd@gmail.com>
2023-06-21 01:57:05 +0530

    ADD: Mysql Database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule} from '@nestjs/typeorm'
+import { ConfigModule} from '@nestjs/config'
+// import { }
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal : true
+    }),
+    TypeOrmModule.forRoot({
+      type : 'mysql',
+      name : 'default',
+      host : process.env.MYSQL_HOST,
+      port : Number(process.env.MYSQL_PORT),
+      username : process.env.MYSQL_USER,
+      password : process.env.MYSQL_PASSWORD,
+      database : process.env.MYSQL_DATABASE,
+      entities : ["dist/orm/*.entity{.ts,.js}"],
+      synchronize : false,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadGatewayException, BadRequestException } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(process.env.PORT);
+  
+  await app.listen(process.env.PORT);
 }
 bootstrap();
