f58d433e78bb7be39cc68d88f02c8c4e92e4cc5b
akshayDhingra01 <a4akshayd@gmail.com>
2023-06-22 01:32:30 +0530

    UPDATE: Logic to create primary or secondary user when got both mail and phone number

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -15,9 +15,7 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
-    console.log("createUserDto");
-    console.log(createUserDto);
-    
+
     const email = createUserDto.email
     const phoneNumber = createUserDto.phoneNumber
 
@@ -57,15 +55,10 @@ export class UsersService {
       
       return "phone but not email"
 
-
       
     } else {
 
       let contacts = await this.getUsers(email, phoneNumber)
-
-      console.log("contacts");
-      console.log(contacts);
-      
 
       if (contacts.length === 0) {
         
@@ -81,26 +74,8 @@ export class UsersService {
         }       
       }
 
-      let primaryContacts = []
+      return this.getResultWhenBothEmailAndNumber(contacts, email, phoneNumber, createUserDto)
 
-      let primaryContactCount = 0
-
-      for (let contact of contacts) {
-        console.log("contact");
-        console.log(contact);
-        if (contact.linkPrecedence === 'primary') {
-          primaryContactCount += 1
-          primaryContacts.push(contact)
-        }        
-      }
-
-      if (primaryContactCount === 1) {
-
-      } else { // create one as primary account
-
-      }
-
-      return 'both are ther'
       }
     
     return 'This action adds a new user';
@@ -109,7 +84,7 @@ export class UsersService {
   async getUsers(email, phoneNumber) {
 
     let contacts
-    console.log(await this.usersRepository.query(`select * from contact`))
+    // console.log(await this.usersRepository.query(`select * from contact`))
     
 
     if (email != null && phoneNumber != null) {
@@ -148,16 +123,128 @@ export class UsersService {
     return this.usersRepository.save(user);
 
   }
+  
+  async createSecondaryContact(createUserDto, primaryContactId) : Promise<User> {
+  
+    const date = new Date(); // Replace this with your actual date or datetime value
+    const formattedDate = format(date, 'yyyy-MM-dd HH:mm:ss');
+    
+    let createdAt = format(date, 'yyyy-MM-dd HH:mm:ss');
+    let updatedAt = format(date, 'yyyy-MM-dd HH:mm:ss');
+    let linkPrecedence = "secondary"
+    let linkedId = primaryContactId
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+    let email = null
+    let phoneNumber = null
+
+    if (createUserDto.email) {
+      email = createUserDto.email
+    }
+
+    if (createUserDto.phoneNumber) {
+      phoneNumber = createUserDto.phoneNumber
+    }
+
+    const user = this.usersRepository.create({ email, phoneNumber, createdAt, updatedAt, linkPrecedence, linkedId });    
+    
+    return this.usersRepository.save(user);
+
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async getResultWhenBothEmailAndNumber(contacts, email, phoneNumber, createUserDto) {
+    
+    let primaryContacts = []
+    let secondaryContactIds = []
+    let secondaryEmailArray = []
+    let secondaryPhoneNumbersArray = []
+
+    let primaryContactCount = 0
+    let newAccountToBeCreated = true
+
+    for (let contact of contacts) {
+
+
+      if (contact.linkPrecedence === 'primary') {
+        primaryContactCount += 1
+        primaryContacts.push(contact)
+      }  else {
+        secondaryContactIds.push(contact.id)
+        secondaryEmailArray.push(contact.email)
+        secondaryPhoneNumbersArray.push(contact.phoneNumber.toString())
+      }
+
+      if (contact.email == email && contact.phoneNumber == phoneNumber) {
+        newAccountToBeCreated = false
+      }     
+    }
+
+    let primaryContactId
+    let emailsArray = []
+    let phoneNumbersArray = []
+    
+
+    if (primaryContactCount === 1) {
+
+      primaryContactId = primaryContacts[0].id
+
+      if (newAccountToBeCreated == true) {
+  
+        let newCreatedUser = await this.createSecondaryContact(createUserDto, primaryContactId)  // As primary contact count is 1
+        secondaryContactIds.push(newCreatedUser.id)
+        secondaryEmailArray.push(newCreatedUser.email)
+        secondaryPhoneNumbersArray.push(newCreatedUser.phoneNumber.toString())
+      
+      }
+      emailsArray.push(primaryContacts[0].email)
+      emailsArray.push(...secondaryEmailArray)
+      
+      phoneNumbersArray.push(primaryContacts[0].phoneNumber)
+      phoneNumbersArray.push(...secondaryPhoneNumbersArray)        
+
+    } else { // create one as primary account
+
+      let firstPrimaryContact = primaryContacts[0]
+      let secondPrimaryContact = primaryContacts[1]
+
+      if (firstPrimaryContact.id < secondPrimaryContact.id) {
+          primaryContactId = firstPrimaryContact.id
+
+          secondaryContactIds.push(secondPrimaryContact.id)
+          secondaryEmailArray.push(secondPrimaryContact.email)
+          secondaryPhoneNumbersArray.push(secondPrimaryContact.phoneNumber.toString())
+          
+          emailsArray.push(firstPrimaryContact.email)
+          emailsArray.push(...secondaryEmailArray)
+          
+          phoneNumbersArray.push(firstPrimaryContact.phoneNumber)
+          phoneNumbersArray.push(...secondaryPhoneNumbersArray)        
+  
+
+      } else {
+          primaryContactId = secondPrimaryContact.id
+
+          secondaryContactIds.push(firstPrimaryContact.id)
+          secondaryEmailArray.push(firstPrimaryContact.email)
+          secondaryPhoneNumbersArray.push(firstPrimaryContact.phoneNumber.toString())
+          
+          emailsArray.push(secondPrimaryContact.email)
+          emailsArray.push(...secondaryEmailArray)
+          
+          phoneNumbersArray.push(secondPrimaryContact.phoneNumber)
+          phoneNumbersArray.push(...secondaryPhoneNumbersArray)        
+  
+      }
+    }
+
+    return {
+      "contact":{
+        "primaryContatctId": primaryContactId,
+        "emails":  [...new Set(emailsArray)], // first element being email of primary contact 
+        "phoneNumbers": [...new Set( phoneNumbersArray)] ,// first element being phoneNumber of primary contact
+        "secondaryContactIds": secondaryContactIds  // Array of all Contact IDs that are "secondary" to the primary contact
+      }
+    }     
+
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
-  }
 }
