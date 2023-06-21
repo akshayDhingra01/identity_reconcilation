9c15100df7ba12ddf09f4ac34e9ea95ce8c202bf
akshayDhingra01 <a4akshayd@gmail.com>
2023-06-21 19:30:59 +0530

    ADD: User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  phoneNumber?: string
+
+  @Column()
+  email?: string
+
+  @Column()
+  linkPrecedence : "secondary"|"primary" // "primary" if it's the first Contact in the link
+
+  @Column()
+  linkedId?: number // the ID of another Contact linked to this one?
+
+  @Column()
+  createdAt: Date;
+
+  @Column()
+  updatedAt: Date;
+
+  @Column()
+  deletedAt?: Date;
+
+  @Column({ default: true })
+  isActive: boolean;
+}
